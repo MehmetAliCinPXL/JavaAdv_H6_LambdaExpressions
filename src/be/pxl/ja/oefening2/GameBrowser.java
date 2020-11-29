3043c7007461425eae2926833d9af541bf97c88d
MehmetAliCinPXL <MehmetAli.Cin@student.pxl.be>
2020-11-29 21:14:08 +0100

    oefening 6.2 finished

--- src/be/pxl/ja/oefening2/GameBrowser.java
+++ src/be/pxl/ja/oefening2/GameBrowser.java
@@ -1,6 +1,7 @@
 package be.pxl.ja.oefening2;
 
 import java.util.List;
+import java.util.function.Predicate;
 
 public class GameBrowser {
 
@@ -12,14 +13,27 @@ public class GameBrowser {
 	}
 
 	public List<VideoGame> showFreeGames() {
-		throw new UnsupportedOperationException("Implement this method");
+		Predicate<VideoGame> filter = v -> v.getPrice() == 0;
+		return gameCollection.selectGame(filter);
 	}
 
 	public List<VideoGame> showGamesInGenre(String action) {
-		throw new UnsupportedOperationException("Implement this method");
+		Predicate<VideoGame> filter = v -> v.getGenres().contains(action);
+		return gameCollection.selectGame(filter);
 	}
 
 	public List<VideoGame> showGamesForSearch(String cd) {
-		throw new UnsupportedOperationException("Implement this method");
+		Predicate<VideoGame> filter = new Predicate<VideoGame>() {
+			@Override
+			public boolean test(VideoGame videoGame) {
+				if (videoGame.getName().toLowerCase().contains(cd.toLowerCase())){
+					return true;
+				}else {
+					return false;
+				}
+			}
+		};
+
+		return gameCollection.selectGame(filter);
 	}
 }
--- src/be/pxl/ja/oefening2/GameCollection.java
+++ src/be/pxl/ja/oefening2/GameCollection.java
@@ -2,12 +2,23 @@ package be.pxl.ja.oefening2;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class GameCollection {
 	private List<VideoGame> videoGames = new ArrayList<>();
 
 	public void addGame(VideoGame game) {
-		throw new UnsupportedOperationException("Implement this method");
+		videoGames.add(game);
+	}
+
+	public List<VideoGame> selectGame(Predicate<VideoGame> filter){
+		List<VideoGame> filteredVideoGames = new ArrayList<>();
+		for (VideoGame v: videoGames) {
+			if (filter.test(v)){
+				filteredVideoGames.add(v);
+			}
+		}
+		return filteredVideoGames;
 	}
 
 }
